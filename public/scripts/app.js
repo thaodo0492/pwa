9d473e7e957662bd8f339ef928bcb864b2b07760
Petyo <petyozh@yahoo.com>
2017-12-26 02:23:11 +0100

    load application and products

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
-(function () {
+var app;(function () {
     'use strict';
 
-    var app = {
+     app = {
         //ELEMENTS
         sp1: $("#senpai1"),
         sp2: $("#senpai2"),
@@ -25,9 +25,43 @@
         //database
         db: firebase.firestore(),
 
+        //data
+        data: {
+            fridgeProducts: undefined
+        },
+        
+
         //is app loading
         isLoading: true
     };
+    
+    //https://stackoverflow.com/a/39914235
+    //https://stackoverflow.com/a/46100662
+    //sleep - A promise awaiter. 
+    //Pass an object with booleans which will be set to true in the promises "then"
+    //Set ms for each time the awaiter checks if the promise was resolved
+    
+    function isBooleanTrue(aBoolean){
+        return aBoolean;
+    }
+    function delay(ms) {
+            return new Promise((resolve, reject) => {
+                    setTimeout(resolve, ms);
+            });
+    }
+    async function sleep(testFlagObj,ms) {
+        //var i = 0;
+        while(true){
+            await delay(ms);
+            var shouldStop = Object.values(testFlagObj).every(isBooleanTrue);
+            if(shouldStop){
+                break;
+            }
+            //console.log(i);
+            //i = i+1;
+        }
+    }
+
 
     /*****************************************************************************
      *
@@ -114,18 +148,143 @@
         }
     }
 
-    function loadMyFridgePage(){
+    //returns a promise that gets products
+    async function loadMyFridgePage(){
+
         toggleLoaderOn(true);
-        $.get( "partials/myFridge.html", function( data ) {
-          $( ".main" ).html( data );
-        }).always(function() {
-            //request finished
-            getProductsCollection().forEach(function(doc){
-                console.log("inside load fridge");
-                console.log(doc.id);
-            });
-            alert( "finished" );
+
+        //things to await
+        var objWaitable = {
+            isFridgeHTMLloaded: false,
+            isProductsDataLoaded: false
+        }
+        var ProductsData;
+
+        //async request for HTML partial
+        $.ajax({
+          url: "partials/myFridge.html"
+        }).done(function( data ) {
+            $(".main").html(data);
+            objWaitable.isFridgeHTMLloaded = true;
         });
+
+        //async request for products data about the logged in users fridge
+        app.db.collection("fridges/"+app.user.uid+"/products").get().then(function(querySnapshot) {
+            ProductsData = querySnapshot;
+            objWaitable.isProductsDataLoaded = true;
+        });
+
+        //wait promises for data and html content to be done
+        await sleep(objWaitable,100);
+
+        var fridgeCollectionUL = document.querySelector('.fridge-collection');
+        //var fridgeCollectionULjQuery = $(".fridge-collection");
+        var observer = new MutationObserver(function(mutations) {});
+        observer.observe(fridgeCollectionUL, { childList: true });
+
+        function deleteElement(element){
+            element.style.display = 'none';
+        }
+        
+        function resetElement(element){
+            element.style.left = 0;
+        }
+
+        function addInteractionToProductsInFridge(mutationRecords){
+            mutationRecords.forEach(function(mutationRecord){
+                //for (var i = 0; i < mutation.addedNodes.length; i++) {
+                //    console.log('"' + mutation.addedNodes[i].textContent + '" added');
+                //}
+                var domObject = mutationRecord.addedNodes[0];
+
+                var hammertime = new Hammer(domObject, {preventDefault: true});
+                hammertime.on("dragup dragdown swipeup swipedown", function(ev){});
+                
+                //do stuff on swipe/drag left/right
+                var newHammer = new Hammer (domObject);
+                newHammer.on("panstart panend panleft panright", function(ev2){
+                    if (ev2.type == "panend"){
+                        if(ev2.deltaX < -120){
+                            deleteElement(domObject);
+                        }else{
+                            resetElement(domObject);
+                        }
+                    }else{
+                        if(ev2.center.x == 0 && ev2.center.y ==0){
+                        }else{
+                            var x = $(".main").find(ev2.target).closest('.removableFridgeItem');
+                            x.css("left",ev2.deltaX+"px");
+                        }   
+                    }
+                });
+            });
+        }
+
+       /* (async () => {
+            var bar = document.createElement('div');
+            bar.textContent = 'bar';
+            fridgeCollectionUL.appendChild(bar);
+            console.log(observer.takeRecords());
+            while(true){
+                await sleep({x:true},1000);
+                (observer.takeRecords()).forEach(function(mutation) {
+                        console.log(mutation);
+                        console.log(mutation.addedNodes);
+                    for (var i = 0; i < mutation.addedNodes.length; i++) {
+                        console.log('"' + mutation.addedNodes[i].textContent + '" added');
+                        var domObject = mutation.addedNodes[i];
+                        var hammertime = new Hammer(domObject, {preventDefault: true});
+                        hammertime.on("dragup dragdown swipeup swipedown", function(ev){});
+
+                        //do stuff on swipe/drag left/right
+                        var newHammer = new Hammer (domObject);
+                        newHammer.on("panstart panend panleft panright", function(ev2){
+                            console.log(ev2);
+                            if (ev2.type == "panend"){
+                                if(ev2.deltaX < -120){
+                                    deleteElement(domObject);
+                                }else{
+                                    resetElement(domObject);
+                                }
+                            }else{
+                                if(ev2.center.x == 0 && ev2.center.y ==0){
+                                }else{
+                                    var x = $(".main").find(ev2.target).closest('.removableFridgeItem');
+                                    x.css("left",ev2.deltaX+"px");
+                                }   
+                            }
+                        });
+                    }
+                });
+            }
+        })();*/
+
+        //Check how many products found in user fridge
+        if(ProductsData.size == 0){
+            //fridge is empty
+            $(".main").addClass("fridge-empty");
+        }else{
+            //fridge has some products
+
+
+
+            var productTemplate = document.querySelector('#templateRemovableFridgeItem');
+
+            var productName = productTemplate.content.querySelector(".fridge-item-name");
+            var productQuantity = productTemplate.content.querySelector(".fridge-item-quantity");
+            var productTime = productTemplate.content.querySelector(".fridge-item-time-left");
+
+            ProductsData.forEach(function(doc) {
+                productName.textContent = doc.data().name;
+                productTime.textContent = doc.data().time;
+                productQuantity.textContent = doc.data().quantity;
+
+                var productTemplateClone = document.importNode(productTemplate.content, true);
+                fridgeCollectionUL.appendChild(productTemplateClone);
+                addInteractionToProductsInFridge(observer.takeRecords());
+            });
+        }
+
         toggleLoaderOn(false);
     }
 
@@ -135,17 +294,6 @@
      * Methods for dealing with the model
      *
      ****************************************************************************/
-     
-     function getProductsCollection(){
-        var collectionProducts;
-        app.db.collection("fridges/"+app.user.uid+"/products").get().then(function(querySnapshot) {
-            collectionProducts = querySnapshot;
-            querySnapshot.forEach(function(doc) {
-                console.log(doc.id, " => ", doc.data());
-            });
-            return collectionProducts;
-        });
-    };
 
 
     /*****************************************************************************
@@ -168,12 +316,10 @@
             isSetupFinished: false
         })
         .then(function() {
-            console.log("User settings successfully created!");
             app.db.collection("fridges").doc(app.user.uid).set({
 
             })
             .then(function(){
-                console.log("User fridge successfully created!");
                 /*app.db.collection("fridges/"+app.user.uid+"/products").add({
 
                 })
@@ -214,7 +360,6 @@
      ************************************************************************/
 
     firebase.auth().onAuthStateChanged(function(user) {
-        toggleLoaderOn(false);
         app.user = user;
         if (user!=undefined && user.emailVerified) {
             if(app.isFirstAuthCheckDone){
@@ -224,45 +369,62 @@
                 app.isFirstAuthCheckDone=true;
                 showSenpai(2);
                 //show senpai2
-                docRefUserSettings = app.db.collection("user-settings").doc(user.uid);
+                app.docRefUserSettings = app.db.collection("user-settings").doc(user.uid);
                 //get the user-settings document
-                docRef.get().then(function(doc) {
+                app.docRefUserSettings.get().then(function(doc) {
                     if (doc.exists) {
                         //check if user finished settup
                             //true
                         if(doc.data().isSetupFinished) {
                                 //load fridge page in senpai2
-                            loadMyFridgePage();
                         }else{
                             loadMyFridgePage();
+
                             //false
                                 // load setup page in senpai2
                         }
                     } else {
-                        console.log("No such user-settings document!");
+
                     }
                 }).catch(function(error) {
-                    console.log("Error getting settings document:", error);
+
                 });
 
             }
         } else if(user && !user.emailVerified) {
-            if(app.isFirstAuthCheckDone=true){ //todelete if
-                setUpAccountData();
-            }//todelete if
+            setUpAccountData();
 
-            //don't verify e-mail just yet
-                app.isFirstAuthCheckDone=true;
-                showSenpai(2);
-                loadMyFridgePage();
-                //check if user finished settup
-                    //true
-                        //load fridge page in senpai2
-                    //false
-                        // load setup page in senpai2
+            //send verification e-mail
+            user.sendEmailVerification().then(function() {
+                //swal
+                swal({
+                    title: 'Successfully registered',
+                    text: "Please verify your e-mail",
+                    type: 'success',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    cancelButtonText: 'Resend confirmation e-mail',
+                    confirmButtonText: 'Open yahoo! mail',
+                    allowOutsideClick: false
+                }).then((result) => {
+                    if (result.value) {
+                        document.location = "http://yahoo.com"
+                    }else if(result.dismiss === 'cancel'){
+                        swal({
+                            type: 'success',
+                            title: 'Your confirmation e-mail has been sent to ...@...com',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                });
+            }).catch(function(error) {
+              // An error happened.
+            });
 
+            
 
-            //swal
             //show a verification screen and continue
 
             /*user.sendEmailVerification().then(function() {
@@ -270,6 +432,7 @@
             }).catch(function(error) {
               // An error happened.
             });*/
+            toggleLoaderOn(false);
         }else{
             if(app.isFirstUnauthCheckDone){
                 //User is signed out
@@ -279,6 +442,7 @@
                 //executes when app runs for the first time if user is not auth.
                 app.isFirstUnauthCheckDone = true;
             }
+            toggleLoaderOn(false);
         }
     });
 
